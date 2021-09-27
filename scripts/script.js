5570bc912cdf0b8aea61f6a1392cc105321fcad3
nebewu <74262987+NeBeWu@users.noreply.github.com>
2021-09-27 00:46:25 -0300

    Implement a reset button for the grid
    Add a button with a onclick call to resetGrid in index.html and
    increase the canvas size to 960px. Refactor the previous code in
    a createGrid function and add a getGridSize, a clearGrid and
    a resetGrid function.
    
    These are all used to implement the reset button for the grid as
    required by the project. Moreover, The refactor helps reutilize
    the previous code in creating new grids, the getGridSize allows
    to get the right input for the grid size, the clearGrid removes
    the previous grid and the resetGrid start the grid resetting
    process.

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,50 @@
 const container = document.querySelector('.container');
 
-let numberColumns = 16;
+createGrid(16);
 
-container.style.gridTemplateColumns = `repeat(${numberColumns}, auto)`;
+function createGrid(numberColumns) {
+  container.style.gridTemplateColumns = `repeat(${numberColumns}, auto)`;
 
-for (i = 0; i < numberColumns * numberColumns; i++) {
-  let cell = document.createElement('div');
-  cell.classList.add('cell');
-  cell.addEventListener("mouseover", changeColor)
-  container.appendChild(cell);
+  for (i = 0; i < numberColumns * numberColumns; i++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.addEventListener('mouseover', changeColor);
+    container.appendChild(cell);
+  }
 }
 
 function changeColor() {
-  this.style.backgroundColor = "black";
+  this.style.backgroundColor = 'black';
+}
+
+function getGridSize() {
+  let answer = prompt(
+    'Please enter the number of rows/columns (min=1, max=100)',
+    16
+  );
+
+  numberColumns = Number(answer);
+
+  while (
+    isNaN(numberColumns) ||
+    numberColumns < 1 ||
+    numberColumns > 100
+  ) {
+    answer = prompt('Please enter the number of rows/columns', 16);
+    numberColumns = Number(answer);
+  }
+
+  return numberColumns;
+}
+
+function clearGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+}
+
+function resetGrid() {
+  clearGrid();
+  let numberColumns = getGridSize();
+  createGrid(numberColumns);
 }
